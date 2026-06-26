9c877831411b974adc54d3ca4dcb4d4d46f2cc4f
Atul <287478177+AtulS-001@users.noreply.github.com>
2026-06-26 21:51:26 +0530

    Day-4 uploaded

---
+++ Day-4/Problem13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    int a = 0, b = 1, c;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    if(n <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 0;
+    }
+
+    printf("Fibonacci Series: ");
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    printf("\n");
+    return 0;
+}
